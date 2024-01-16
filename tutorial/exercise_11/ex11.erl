9ae82f673610b15a36607f63b95dcfaef2a67757
Florian Katschenka <stud100611@P03-PC07-TUX.ad.fh-wedel.de>
2024-01-16 16:31:29 +0100

    finisch

--- tutorial/exercise_11/ex11.erl
+++ tutorial/exercise_11/ex11.erl
@@ -19,19 +19,20 @@ rpc(Pid, Request, C) ->
     end.
 
 
-sendElection(Pid, [H|T], Group, C) ->
+sendElection(Pid, [H|T], Group, OkCount, C) ->
     if 
-        Pid > H -> sendElection(Pid, T, Group, C);
+        Pid > H -> sendElection(Pid, T, Group, OkCount, C);
         true -> io:format("~p: sending election message to: ~p ~n", [Pid, H]),
                 case rpc(H, election, C) of
-                    ok -> io:format("~p: got ok from: ~p ~n", [Pid, H]), ok;
-                    unreachable -> io:format("~p: got unreachable from: ~p ~n", [Pid, H]), sendElection(Pid, T, Group, C)
+                    ok -> io:format("~p: got ok from: ~p ~n", [Pid, H]), sendElection(Pid, T, Group, OkCount + 1, C);
+                    unreachable -> io:format("~p: got unreachable from: ~p ~n", [Pid, H]), sendElection(Pid, T, Group, OkCount, C)
                 end
     end;
-sendElection(Pid, [], Group, _) ->
-    sendCoordinator(Pid, Group), 
-    unreachable.
-
+sendElection(Pid, [], Group, OkCount, _) ->
+    if 
+        OkCount == 0 -> sendCoordinator(Pid, Group), coordinator;
+        true -> ok
+    end.
 
 sendCoordinator(Pid, [H|T]) ->
     io:format("~p: sending coordinator message to: ~p ~n", [Pid, H]),
@@ -49,19 +50,28 @@ addToGroup(Pid, Group) ->
 
 % The state of each process contains at least the current coordinator (maybe the own election
 % value) and also a list of all other processes in the group.
-process(undefined, Group, C) ->
-    case sendElection(self(), Group, Group, C) of 
-        unreachable -> process(self(), Group, C);
+process(undefined, Group, C, init) ->
+    case sendElection(self(), Group, Group, 0, C) of 
+        coordinator -> process(self(), Group, C, normal);
         ok ->
             receive
-                {coordinator, Pid} -> process(Pid, Group, C)
+                {coordinator, Pid} -> process(Pid, Group, C, normal)
             end
     end;
-process(Coordinator, Group, C) ->
+process(Coordinator, Group, C, electing) ->
     receive
-        startElection -> sendElection(self(), Group, Group, C), process(Coordinator, Group, C);
-        {Pid, election} -> Pid ! {self(), ok}, sendElection(self(), Group, Group, C), process(Coordinator, addToGroup(Pid, Group), C);
-        {coordinator, Pid} -> process(Pid, addToGroup(Pid, Group), C);
+        %startElection -> sendElection(self(), Group, Group, 0, C), process(Coordinator, Group, C);
+        {Pid, election} -> Pid ! {self(), ok}, process(Coordinator, addToGroup(Pid, Group), C, electing);
+        {coordinator, Pid} -> process(Pid, addToGroup(Pid, Group), C, normal);
+        stop -> ok
+    after 1000 -> 
+        process(Coordinator, Group, C, normal)
+    end;
+process(Coordinator, Group, C, normal) ->
+    receive
+        startElection -> sendElection(self(), Group, Group, 0, C), process(Coordinator, Group, C, electing);
+        {Pid, election} -> Pid ! {self(), ok}, sendElection(self(), Group, Group, 0, C), process(Coordinator, addToGroup(Pid, Group), C, electing);
+        {coordinator, Pid} -> process(Pid, addToGroup(Pid, Group), C, normal);
         stop -> ok
     end.
 
@@ -70,15 +80,15 @@ setupCounter() ->
     spawn(fun() -> counter(0) end).
 
 setup(Counter) ->
-    P1 = spawn(fun() -> process(undefined, [], Counter) end),
-    P2 = spawn(fun() -> process(undefined, [P1], Counter) end),
-    P3 = spawn(fun() -> process(undefined, [P1,P2], Counter) end),
-    P4 = spawn(fun() -> process(undefined, [P1,P2,P3], Counter) end),
-    P5 = spawn(fun() -> process(undefined, [P1,P2,P3,P4], Counter) end),
+    P1 = spawn(fun() -> process(undefined, [], Counter, init) end),
+    P2 = spawn(fun() -> process(undefined, [P1], Counter, init) end),
+    P3 = spawn(fun() -> process(undefined, [P1,P2], Counter, init) end),
+    P4 = spawn(fun() -> process(undefined, [P1,P2,P3], Counter, init) end),
+    P5 = spawn(fun() -> process(undefined, [P1,P2,P3,P4], Counter, init) end),
     [P1,P2,P3,P4,P5].
 
 startOne(Group, Counter) ->
-    spawn(fun() -> process(undefined, Group, Counter) end).
+    spawn(fun() -> process(undefined, Group, Counter, init) end).
 
 stopSome(Group) ->
     lists:nth(5, Group) ! stop, 
@@ -90,3 +100,5 @@ startElection(Pid) ->
 %C = ex11:setupCounter().
 %G = ex11:setup(C).
 %ex11:startElection(lists:nth(1,G)).
+
+%C = ex11:setupCounter(), G = ex11:setup(C), ex11:startElection(lists:nth(1,G)).
