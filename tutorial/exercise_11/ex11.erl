2f7fd99477ed2115cac07dff857031c38593061b
Florian Katschenka <stud100611@P01-PC33-TUX.ad.fh-wedel.de>
2024-01-16 13:58:32 +0100

    eigentlich fertig

--- tutorial/exercise_11/ex11.erl
+++ tutorial/exercise_11/ex11.erl
@@ -2,8 +2,15 @@
 -import(lists, [append/2, member/2]).
 -compile(export_all).
 
-rpc(Pid, Request) -> 
-    Pid ! {self(), Request}, 
+
+counter(ElectionCnt) -> 
+    receive
+        election -> counter(ElectionCnt + 1);
+        getCount -> io:format("~p election messages total ~n", [ElectionCnt]), counter(ElectionCnt)
+    end.
+
+rpc(Pid, Request, C) -> 
+    Pid ! {self(), Request}, C ! Request,
     receive 
        {Pid, Response} -> 
            Response
@@ -12,15 +19,16 @@ rpc(Pid, Request) ->
     end.
 
 
-sendElection(Pid, [H|T], Group) ->
+sendElection(Pid, [H|T], Group, C) ->
     if 
-        self() > H -> sendElection(Pid, T, Group);
-        true -> case rpc(H, election) of
-                    ok -> ok;
-                    unreachable -> sendElection(Pid, T, Group)
+        Pid > H -> sendElection(Pid, T, Group, C);
+        true -> io:format("~p: sending election message to: ~p ~n", [Pid, H]),
+                case rpc(H, election, C) of
+                    ok -> io:format("~p: got ok from: ~p ~n", [Pid, H]), ok;
+                    unreachable -> io:format("~p: got unreachable from: ~p ~n", [Pid, H]), sendElection(Pid, T, Group, C)
                 end
     end;
-sendElection(Pid, [], Group) ->
+sendElection(Pid, [], Group, _) ->
     sendCoordinator(Pid, Group), 
     unreachable.
 
@@ -29,7 +37,7 @@ sendCoordinator(Pid, [H|T]) ->
     io:format("~p: sending coordinator message to: ~p ~n", [Pid, H]),
     H ! {coordinator, Pid}, sendCoordinator(Pid, T);
 sendCoordinator(Pid, []) ->
-    io:format("~p: all coordinator messages sent: ~n", [Pid]).
+    io:format("~p: all coordinator messages sent. ~n", [Pid]).
     
 
 addToGroup(Pid, Group) ->
@@ -41,23 +49,44 @@ addToGroup(Pid, Group) ->
 
 % The state of each process contains at least the current coordinator (maybe the own election
 % value) and also a list of all other processes in the group.
-process(undefined, Group) ->
-% hold election
-    case sendElection(self(), Group, Group) of 
-        unreachable -> process(self(), Group);
+process(undefined, Group, C) ->
+    case sendElection(self(), Group, Group, C) of 
+        unreachable -> process(self(), Group, C);
         ok ->
             receive
-                {coordinator, Pid} -> process(Pid, Group)
+                {coordinator, Pid} -> process(Pid, Group, C)
             end
     end;
-process(Coordinator, Group) ->
+process(Coordinator, Group, C) ->
     receive
-        {election, Pid} -> Pid ! ok, sendElection(self(), Group, Group), process(Coordinator, addToGroup(Pid, Group));
-        {coordinator, Pid} -> process(Pid, Group)
+        startElection -> sendElection(self(), Group, Group, C), process(Coordinator, Group, C);
+        {Pid, election} -> Pid ! {self(), ok}, sendElection(self(), Group, Group, C), process(Coordinator, addToGroup(Pid, Group), C);
+        {coordinator, Pid} -> process(Pid, addToGroup(Pid, Group), C);
+        stop -> ok
     end.
 
 
+setupCounter() -> 
+    spawn(fun() -> counter(0) end).
 
-setup() ->
-    P1 = spawn(fun() -> process(undefined, []) end),
-    P2 = spawn(fun() -> process(undefined, [P1]) end).
+setup(Counter) ->
+    P1 = spawn(fun() -> process(undefined, [], Counter) end),
+    P2 = spawn(fun() -> process(undefined, [P1], Counter) end),
+    P3 = spawn(fun() -> process(undefined, [P1,P2], Counter) end),
+    P4 = spawn(fun() -> process(undefined, [P1,P2,P3], Counter) end),
+    P5 = spawn(fun() -> process(undefined, [P1,P2,P3,P4], Counter) end),
+    [P1,P2,P3,P4,P5].
+
+startOne(Group, Counter) ->
+    spawn(fun() -> process(undefined, Group, Counter) end).
+
+stopSome(Group) ->
+    lists:nth(5, Group) ! stop, 
+    lists:nth(2, Group) ! stop.
+
+startElection(Pid) ->
+    Pid ! startElection.
+
+%C = ex11:setupCounter().
+%G = ex11:setup(C).
+%ex11:startElection(lists:nth(1,G)).
